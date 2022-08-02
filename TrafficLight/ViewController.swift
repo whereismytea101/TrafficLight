26506934e9cc7761dd8d4e1f96c0c5613b7a71c2
Samir <imnotajedi@icloud.com>
2022-08-02 23:03:12 +0300

    1. Major changes in start button logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-let currentAlpha: CGFloat = 0.3
+private let lightsOff: CGFloat = 0.3
+private let lightsOn: CGFloat = 1
 
 
 class ViewController: UIViewController {
@@ -15,37 +16,59 @@ class ViewController: UIViewController {
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
+    
     @IBOutlet var button: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redView.alpha = currentAlpha
-        yellowView.alpha = currentAlpha
-        greenView.alpha = currentAlpha
         button.layer.cornerRadius = 20
         
-        redView.layer.cornerRadius = 64
-        yellowView.layer.cornerRadius = 64
-        greenView.layer.cornerRadius = 64
+        redView.alpha = lightsOff
+        yellowView.alpha = lightsOff
+        greenView.alpha = lightsOff
         
-
+        print("Размер стороны, доступный в методе viewDidLoad: \(redView.frame.height)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        
+        print("Размер стороны, доступный в методе viewWillLayoutSubviews: \(redView.frame.height)")
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         button.setTitle("Next", for: .normal)
         
-        if redView.alpha == yellowView.alpha {
-            redView.alpha = CGFloat(MAXFLOAT)
-            greenView.alpha = currentAlpha
-        } else if redView.alpha > currentAlpha && yellowView.alpha == greenView.alpha {
-            yellowView.alpha = CGFloat(MAXFLOAT)
-            redView.alpha = currentAlpha
-        } else if yellowView.alpha > currentAlpha && greenView.alpha == redView.alpha {
-            greenView.alpha = CGFloat(MAXFLOAT)
-            yellowView.alpha = currentAlpha
+        if redView.alpha == 1 {
+            redView.alpha = lightsOff
+            yellowView.alpha = lightsOn
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = lightsOff
+            greenView.alpha = lightsOn
+        } else if greenView.alpha == 1 {
+            greenView.alpha = lightsOff
+            redView.alpha = lightsOn
+        } else {
+            redView.alpha = lightsOn
         }
+        
+//        Мое решение
+        
+//        if redView.alpha == yellowView.alpha {
+//            redView.alpha = CGFloat(MAXFLOAT)
+//            greenView.alpha = currentAlpha
+//        } else if redView.alpha > currentAlpha && yellowView.alpha == greenView.alpha {
+//            yellowView.alpha = CGFloat(MAXFLOAT)
+//            redView.alpha = currentAlpha
+//        } else if yellowView.alpha > currentAlpha && greenView.alpha == redView.alpha {
+//            greenView.alpha = CGFloat(MAXFLOAT)
+//            yellowView.alpha = currentAlpha
+//        }
     }
     
 }
